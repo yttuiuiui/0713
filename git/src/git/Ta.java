6be979f51c6c0695de1f97fda76055166087ef84
aa <bb>
2017-07-13 09:18:00 +0800

    第一次提交

--- git/src/git/Ta.java
+++ git/src/git/Ta.java
@@ -5,10 +5,12 @@ public class Ta {
 	int y=2;
 	int z=3;
 	int p=9;
+	int s=3;
 	public int sum(int x,int y,int z){
 		return x+y+z;
 	}
    public static void main(String[] args) {
+	   //
 	
 }
 }
